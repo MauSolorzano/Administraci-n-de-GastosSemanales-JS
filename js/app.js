debd5e96dbc3a10ba53bf0c88301155dfd156c6d
MauSolorzano <mauriciosolorzano93@gmail.com>
2026-03-02 10:55:28 -0600

    Update app.js
    Se agrega la funcionalidad que el usuario elija si puede exceder el monto del presupuesto inicial

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const formulario = document.querySelector('#agregar-gasto');
 const gastoListado = document.querySelector('#gastos ul');
 let presupuesto;
-
+let exederPresupuesto = false;
 //Eventos 
 eventListeners();
 function eventListeners(){
@@ -30,6 +30,10 @@ class Presupuesto{
         this.restante = this.presupuesto - gastado;
         console.log(gastado)
     }
+
+    obtenerRestante(){
+        return this.restante;
+    }
 }
 
 class UI{
@@ -44,11 +48,43 @@ class UI{
 
     imprimirAlerta(mensaje, tipo){
         const divMensaje = document.createElement('div');
-        divMensaje.classList.add('text-center', 'alert');
 
+        
+        divMensaje.classList.add('text-center', 'alert');
+         
         if(tipo === 'error'){
             divMensaje.classList.add('alert-danger')
-        }else{
+
+        }else if(tipo === 'error-restante'){
+
+            divMensaje.classList.add('alert-warning');
+            console.log(document.getElementById('agregarExedentes'));
+            if (!document.getElementById('agregarExcedentes')){
+                const btnAgregarGastosExcedentes = document.createElement('button');
+
+                // 2. Configurar propiedades
+                btnAgregarGastosExcedentes.innerText = 'Seguir agregando';
+                btnAgregarGastosExcedentes.id = 'agregarExcedentes';
+                btnAgregarGastosExcedentes.className = 'btn btn-submit btn-primary';
+
+                btnAgregarGastosExcedentes.onclick = ()=> {
+
+                    exederPresupuesto = true;
+                    this.imprimirAlerta('Se habilito el agregar excedentes \n por favor intente de nuevo');
+
+                };
+
+                
+                // 3. Insertar el botón en el DOM (ej. en el body o un div específico)
+                document.querySelector('.primario').insertBefore(btnAgregarGastosExcedentes, formulario);
+
+                setTimeout(()=>{
+                    btnAgregarGastosExcedentes.remove();
+                }, 5000);
+            }
+            
+        }
+        else{
             divMensaje.classList.add('alert-success')
         }
 
@@ -60,6 +96,8 @@ class UI{
         setTimeout(() => {
             divMensaje.remove();
         }, 3000);
+
+        
     }
     agregarGastoListado(gastos){
         const nuevoGasto = document.createElement('li');
@@ -93,6 +131,24 @@ class UI{
     actualizarRestante(restante){
         document.querySelector('#restante').textContent = restante;
     }
+    
+    comprobarPresupuesto(presupuestoObj){
+        const {presupuesto, restante} = presupuestoObj;
+        const restanteDiv = document.querySelector('.restante');
+
+        if((presupuesto / 4)> restante){
+
+            restanteDiv.classList.remove('alert-success');
+            restanteDiv.classList.remove('alert-warning');
+            restanteDiv.classList.add('alert-danger');
+
+        }else if((presupuesto / 2)> restante){
+
+            restanteDiv.classList.remove('alert-success','alert-danger');
+            restanteDiv.classList.add('alert-warning');
+        }
+
+    }
 }
 
 //Instaciar UI
@@ -115,28 +171,33 @@ function agregarGastos(e){
     //Leer los datos del formulario
     const nombre = document.querySelector('#gasto').value;
     const cantidad = Number( document.querySelector('#cantidad').value);
-
+    const restanteValidacion = presupuesto.obtenerRestante();
     if(nombre === "" || cantidad === ""){
         ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
         return;
     }else if(isNaN(cantidad) || cantidad<=0){
         ui.imprimirAlerta('Cantidad no Valida', 'error');
         return;
+    }else if(restanteValidacion<cantidad && exederPresupuesto === false){
+        ui.imprimirAlerta('Cantidad excede el monto restante del presupuesto', 'error-restante');
+        return;
     }
-
 
     //Generar un objeto con el gasto 
     //Object literal (al reves que el distructor)
     const gasto = {nombre, cantidad, id: Date.now()};
     //Date now es una forma sencilla de crear un id únici
 
+
     presupuesto.nuevoGasto(gasto);
+    
+    const {gastos, restante} = presupuesto;
+
 
     ui.imprimirAlerta('Gasto agregado correctamente', 'correcto');
-
-    const {gastos, restante} = presupuesto;
-    //Imprimir los gastos 
+    //Imprimir los gastos  
     ui.agregarGastoListado(gastos);
     ui.actualizarRestante(restante);
+    ui.comprobarPresupuesto(presupuesto);
     formulario.reset();
 }
